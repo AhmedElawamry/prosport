57edca4d0cc81d654027d9794c7b4a1811995533
AhmedElawamry <32811650+AhmedElawamry@users.noreply.github.com>
2019-12-03 00:24:08 +0200

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,17 @@
-function morebtn(){
-    var less = document.getElementsByClassName("less");
-    var more = document.getElementsByClassName("more");
-    var btnTxt = document.getElementsByClassName("mbtn");
-
-    if (less.style.display == "none"){
-        less.style.display = "inline";
-        btnTxt = "Read More";
-        more.style.display = "none";
-    }
-    else{
-        less.style.display = "none";
-        btnTxt.innerHTML = "Read Less";
-        more.style.display = "inline";
-    }
-}
-
-function des(){
-    var d = document.getElementById("design-your");
-
-}
+$(document).ready(function() {
+    $("#toggle").click(function() {
+        var elem = $("#toggle").text();
+        if (elem == "Read More") {
+            //Stuff to do when btn is in the read more state
+            $("#toggle").text("More Info");
+            $("#text").slideDown();
+        } else {
+            //Stuff to do when btn is in the read less state
+            $("#toggle").text("More Info");
+            $("#text").slideUp();
+        }
+    });
+});
 
 
 $(window).load(function(){
