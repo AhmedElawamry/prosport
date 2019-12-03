c3b620530781a0dfef08825de582e4e845ef3da4
AhmedElawamry <32811650+AhmedElawamry@users.noreply.github.com>
2019-12-03 21:26:49 +0200

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,18 @@
-$('.moreless-button').click(function() {
-    $('.moretext').slideToggle();
-    if ($('.moreless-button').text() == "Read more") {
-        $(this).text("Read less")
-    } else {
-        $(this).text("Read more")
-    }
+$(document).ready(function() {
+    var hiddenInfo = $(".hidden-info");
+    var readMoreTrigger = $(".read-more-trigger");
+
+    //hide div .hidden-info
+    hiddenInfo.hide();
+
+    readMoreTrigger.on("click", function(){
+        //toggle the hidden div
+        hiddenInfo.toggle();
+        //toggle text in .hidden-info to 'Read Less', 'Read More'
+        if (readMoreTrigger.text() == "Read More")
+            readMoreTrigger.text("Read less");
+        else readMoreTrigger.text("Read More");
+    });
 });
 
 (function($) {
